8f7cea2a7ca30ef5f6762b4f092f36cec9a4cf5b
Hestia Lestari <hestialestari71@gmail.com>
2026-05-02 11:26:55 +0700

    Update rendering structure in main.jsx
    Replaced React.StrictMode with a styled div containing text.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './rekain-store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{padding:40, fontsize:24}}>
+    React Jalan
+    </div>
 )
