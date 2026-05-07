61dcd8dda8c39fb78603bb3f6f2013c31631e315
Hestia Lestari <hestialestari71@gmail.com>
2026-05-08 00:25:35 +0700

    Fix import statement formatting in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RekainFashion from './rekain-store.jsx' 
+import RekainFashion from './rekain-store.jsx'  
 
 const rootElement = document.getElementById('root');
 
