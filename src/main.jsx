80c554688ec4306e5b6343e8ee9d401d64b5c334
Hestia Lestari <hestialestari71@gmail.com>
2026-05-07 23:39:34 +0700

    Update import statement and add root element check
    Added .jsx extension to import statement and added a check for the root element's existence before rendering.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RekainStore from './rekain-store' // Pastikan nama filenya benar
+import RekainStore from './rekain-store.jsx' // Menambahkan ekstensi .jsx agar lebih pasti
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RekainStore />
-  </React.StrictMode>,
-)
+// Pastikan element dengan id 'root' ada di index.html
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <RekainStore />
+    </React.StrictMode>,
+  )
+} else {
+  console.error("Elemen root tidak ditemukan! Pastikan index.html memiliki <div id='root'></div>");
+}
