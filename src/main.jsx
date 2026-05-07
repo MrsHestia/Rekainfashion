cce878bf22e5e06005efb1ecdf6c32e3046c7ab5
Hestia Lestari <hestialestari71@gmail.com>
2026-05-07 23:52:09 +0700

    Change component from RekainStore to RekainFashion
    Updated component import and usage from RekainStore to RekainFashion.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RekainStore from './rekain-store.jsx' // Menambahkan ekstensi .jsx agar lebih pasti
-import './index.css'
+// Ubah dari RekainStore menjadi RekainFashion agar sesuai dengan isi filenya
+import RekainFashion from './rekain-store.jsx' 
 
-// Pastikan element dengan id 'root' ada di index.html
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <RekainStore />
+      <RekainFashion />
     </React.StrictMode>,
   )
-} else {
-  console.error("Elemen root tidak ditemukan! Pastikan index.html memiliki <div id='root'></div>");
 }
